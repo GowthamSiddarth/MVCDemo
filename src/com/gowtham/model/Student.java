716e6cea9358299dc2d17a9009b7e53d7bb995c4
Gowtham Behara <gothsiddu@gmail.com>
2019-08-25 12:25:28 +0530

    Create model for Student

---
+++ src/com/gowtham/model/Student.java
@@ -0,0 +1,22 @@
+package com.gowtham.model;
+
+public class Student {
+    private String name;
+    private String rollNum;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRollNum(String rollNum) {
+        this.rollNum = rollNum;
+    }
+
+    public String getRollNum() {
+        return rollNum;
+    }
+}
