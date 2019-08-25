1ee71a5b44a6bcdb57abae07bec957087591f7a1
Gowtham Behara <gothsiddu@gmail.com>
2019-08-25 12:38:04 +0530

    Use main to init model, view and controller to demonstrate MVC pattern

--- src/com/gowtham/MVCDemo.java
+++ src/com/gowtham/MVCDemo.java
@@ -1,8 +1,28 @@
 package com.gowtham;
 
+import com.gowtham.controller.StudentController;
+import com.gowtham.model.Student;
+import com.gowtham.view.StudentView;
+
 public class MVCDemo {
 
     public static void main(String[] args) {
-	// write your code here
+        Student student = loadStudent();
+        StudentView studentView = new StudentView();
+
+        StudentController studentController = new StudentController(student, studentView);
+        studentController.updateStudentView();
+
+        studentController.setStudentName("Gowtham");
+        studentController.setStudentRollNum("506");
+        studentController.updateStudentView();
+    }
+
+    static Student loadStudent() {
+        Student student = new Student();
+        student.setRollNum("1");
+        student.setName("gowtham");
+
+        return student;
     }
 }
